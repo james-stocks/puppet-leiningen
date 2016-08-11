3a7fe9d90d6fea5e9bbcab2b2699fa731bcc0717
James Stocks <james.stocks@puppet.com>
2016-08-11 13:20:39 +0000

    (maint) Fix resource dependency in bootstrap_fetch.pp

--- manifests/bootstrap_fetch.pp
+++ manifests/bootstrap_fetch.pp
@@ -1,16 +1,17 @@
 class leiningen::bootstrap_fetch inherits leiningen {
   include wget
 
-  wget::fetch { "Fetch lein bootstrap":
+  wget::fetch { 'Fetch lein bootstrap':
     source      => $fetch_url,
     destination => "${bin_dir}/lein",
     timeout     => $fetch_timeout,
     verbose     => false,
     unless      => "test -f ${bin_dir}/lein",
-    notify    => File["${bin_dir}/lein"],
+    notify      => File['lein bootstrap file'],
   }
 
-  File { "${bin_dir}/lein":
-    mode => "a+x",    
+  File { 'lein bootstrap file':
+    path        => "${bin_dir}/lein",
+    mode        => 'a+x',
   }
 }
