232bc2ff566a9cfd862b4566c21f7609043ed384
James Stocks <james.stocks@puppet.com>
2016-08-11 13:29:05 +0000

    (maint) Fix lint issues in bootstrap_fetch.pp

--- manifests/bootstrap_fetch.pp
+++ manifests/bootstrap_fetch.pp
@@ -2,16 +2,16 @@ class leiningen::bootstrap_fetch inherits leiningen {
   include wget
 
   wget::fetch { 'Fetch lein bootstrap':
-    source      => $fetch_url,
-    destination => "${bin_dir}/lein",
-    timeout     => $fetch_timeout,
+    source      => $leiningen::fetch_url,
+    destination => "${leiningen::bin_dir}/lein",
+    timeout     => $leiningen::fetch_timeout,
     verbose     => false,
-    unless      => "test -f ${bin_dir}/lein",
+    unless      => "test -f ${leiningen::bin_dir}/lein",
     notify      => File['lein bootstrap file'],
   }
 
   File { 'lein bootstrap file':
-    path        => "${bin_dir}/lein",
-    mode        => 'a+x',
+    path => "${leiningen::bin_dir}/lein",
+    mode => 'a+x',
   }
 }
